542d600a4583ef264b830dc700f5d1e6b88f81bf
HushABF <nkbh12342@gmail.com>
2026-04-07 21:09:57 +0300

    Update app router to pass product model to ProductDetailScreen and clean up imports in CategoryListScreen

--- lib/features/category/screens/category_list_screen.dart
+++ lib/features/category/screens/category_list_screen.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 import 'package:product_browser_app/core/di/service_locator.dart';
-import 'package:product_browser_app/core/widgets/cart_badge_button.dart';
-import 'package:product_browser_app/core/widgets/error_view.dart';
 import 'package:product_browser_app/features/category/cubit/category_cubit.dart';
-import 'package:product_browser_app/features/category/data/model/category_model.dart';
-import 'package:product_browser_app/features/category/screens/widgets/category_card.dart';
-import 'package:product_browser_app/features/category/screens/widgets/category_grid.dart';
 import 'package:product_browser_app/features/category/screens/widgets/category_view.dart';
 import 'package:product_browser_app/features/product/data/product_repository.dart';
 
