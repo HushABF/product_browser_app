af267ca7f5bd6a785e36179fb5ff76eef062a9db
HushABF <nkbh12342@gmail.com>
2026-04-07 18:00:23 +0300

    Add ProductModel, Dimensions, Meta, and Review classes for product data representation

---
+++ lib/features/product/data/model/product_model/meta.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+
+class Meta extends Equatable {
+	final DateTime createdAt;
+	final DateTime updatedAt;
+	final String barcode;
+	final String qrCode;
+
+	const Meta({required this.createdAt, required this.updatedAt, required this.barcode, required this.qrCode});
+
+	factory Meta.fromJson(Map<String, dynamic> json) => Meta(
+				createdAt: DateTime.parse(json['createdAt'] as String),
+				updatedAt: DateTime.parse(json['updatedAt'] as String),
+				barcode: json['barcode'] as String,
+				qrCode: json['qrCode'] as String,
+			);
+
+	Map<String, dynamic> toJson() => {
+				'createdAt': createdAt.toIso8601String(),
+				'updatedAt': updatedAt.toIso8601String(),
+				'barcode': barcode,
+				'qrCode': qrCode,
+			};
+
+	@override
+	List<Object> get props => [createdAt, updatedAt, barcode, qrCode];
+}
