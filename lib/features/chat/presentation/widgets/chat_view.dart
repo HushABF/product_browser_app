fb67b3ab9b7f292cfa3ec0bfdd52fc8836f0e7a9
HushABF <nkbh12342@gmail.com>
2026-04-18 22:49:38 +0300

    feat: enhance ChatView and ProductEntity to include product rating and improve UI layout

--- lib/features/chat/presentation/widgets/chat_view.dart
+++ lib/features/chat/presentation/widgets/chat_view.dart
@@ -7,6 +7,36 @@ class ChatView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text(product.title)));
+    var colorScheme = Theme.of(context).colorScheme;
+    var textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          product.title,
+          style: textTheme.titleSmall!.copyWith(fontWeight: .bold),
+        ),
+      ),
+      body: Column(
+        children: [
+          ListTile(
+            // tileColor: Colors.grey.shade200,
+            tileColor: colorScheme.secondaryFixed,
+            leading: Image.network(product.thumbnail),
+            title: Text(
+              product.title,
+              style: textTheme.bodySmall!.copyWith(fontWeight: .normal),
+            ),
+            subtitle: Text('\$${product.price.toString()}'),
+            trailing: Text(
+              'Rating: ${product.rating}',
+              style: textTheme.bodySmall,
+            ),
+          ),
+          Expanded(child: ListView.builder(itemBuilder: (context, index) {
+            return ListTile()
+          },))
+        ],
+      ),
+    );
   }
 }
