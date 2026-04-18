286b64fcd25145e00c681232e4c18e6181f02ef0
HushABF <nkbh12342@gmail.com>
2026-04-18 16:47:08 +0300

    feat: add chat repository and use cases to service locator

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,8 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get_it/get_it.dart';
 import 'package:product_browser_app/core/network/dio_client.dart';
 import 'package:product_browser_app/features/cart/data/cart_repository_impl.dart';
 import 'package:product_browser_app/features/cart/domain/repositories/cart_repository.dart';
 import 'package:product_browser_app/features/cart/domain/usecases/get_cart_use_case.dart';
+import 'package:product_browser_app/features/chat/data/repositories/chat_repository_impl.dart';
+import 'package:product_browser_app/features/chat/domain/repositories/chat_repository.dart';
+import 'package:product_browser_app/features/chat/domain/usecases/send_message_use_case.dart';
+import 'package:product_browser_app/features/chat/domain/usecases/watch_messages_use_case.dart';
 import 'package:product_browser_app/features/product/data/product_repository_impl.dart';
 import 'package:product_browser_app/features/product/domain/repositories/product_repository.dart';
 import 'package:product_browser_app/features/category/data/category_repository_impl.dart';
@@ -13,19 +18,40 @@ import 'package:product_browser_app/features/product/domain/usecases/get_product
 final GetIt getIt = GetIt.instance;
 
 Future<void> setupLocator() async {
+  // Core
   getIt.registerLazySingleton(() => DioClient());
+  getIt.registerLazySingleton<FirebaseFirestore>(
+    () => FirebaseFirestore.instance,
+  );
+
+  // Product
   getIt.registerLazySingleton<ProductRepository>(
     () => ProductRepositoryImpl(getIt()),
   );
   getIt.registerLazySingleton(
     () => GetProductsByCategoryUseCase(getIt<ProductRepository>()),
   );
+
+  // Category
   getIt.registerLazySingleton<CategoryRepository>(
     () => CategoryRepositoryImpl(getIt()),
   );
   getIt.registerLazySingleton(
     () => GetCategoriesUseCase(getIt<CategoryRepository>()),
   );
+
+  // Cart
   getIt.registerLazySingleton<CartRepository>(() => CartRepositoryImpl());
   getIt.registerLazySingleton(() => GetCartUseCase(getIt<CartRepository>()));
+
+  // Chat
+  getIt.registerLazySingleton<ChatRepository>(
+    () => ChatRepositoryImpl(firestore: getIt()),
+  );
+  getIt.registerLazySingleton(
+    () => WatchMessagesUseCase(chatRepository: getIt()),
+  );
+  getIt.registerLazySingleton(
+    () => SendMessageUseCase(chatRepository: getIt()),
+  );
 }
