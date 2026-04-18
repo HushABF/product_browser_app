fbec6085f3343928b9fb992f0d727e61adfc23d9
HushABF <nkbh12342@gmail.com>
2026-04-18 15:43:35 +0300

    feat: implement cloud_firestore integration and message model for chat feature

---
+++ lib/features/chat/data/model/message_model.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:product_browser_app/features/chat/domain/entities/message_entity.dart';
+
+class MessageModel extends MessageEntity {
+  const MessageModel({
+    required super.id,
+    required super.productId,
+    required super.senderUsername,
+    required super.text,
+    required super.createdAt,
+  });
+
+  factory MessageModel.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return MessageModel(
+      id: doc.id,
+      productId: data['productId'],
+      senderUsername: data['senderUsername'],
+      text: data['text'],
+      createdAt: (data['createdAt'] as Timestamp?)?.toDate() ?? DateTime.now(),
+    );
+  }
+
+  Map<String, dynamic> toMap() => {
+    'productId': productId,
+    'senderUsername': senderUsername,
+    'text': text,
+    'createdAt': FieldValue.serverTimestamp(),
+  };
+}
