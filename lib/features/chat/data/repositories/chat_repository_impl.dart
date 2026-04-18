505007345f7b3f2cb63e7fd0f3dcc8ed651328f6
HushABF <nkbh12342@gmail.com>
2026-04-18 16:34:02 +0300

    feat: implement ChatRepository for managing messages in chat feature

---
+++ lib/features/chat/data/repositories/chat_repository_impl.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:product_browser_app/features/chat/data/model/message_model.dart';
+import 'package:product_browser_app/features/chat/domain/entities/message_entity.dart';
+import 'package:product_browser_app/features/chat/domain/repositories/chat_repository.dart';
+
+class ChatRepositoryImpl implements ChatRepository {
+  final FirebaseFirestore firestore;
+
+  ChatRepositoryImpl({required this.firestore});
+  @override
+  Stream<List<MessageEntity>> getMessages({required String productId}) {
+    return firestore
+        .collection('chats')
+        .doc(productId)
+        .collection('messages')
+        .snapshots()
+        .map(
+          (snapshot) => snapshot.docs
+              .map((doc) => MessageModel.fromFirestore(doc))
+              .toList(),
+        );
+  }
+
+  @override
+  Future<void> sendMessage({
+    required String productId,
+    required String senderUsername,
+    required String text,
+  }) async {
+    await firestore
+        .collection('chats')
+        .doc(productId)
+        .collection('messages')
+        .add({
+          'productId': productId,
+          'senderUsername': senderUsername,
+          'text': text,
+          'createdAt': FieldValue.serverTimestamp(),
+        });
+  }
+}
