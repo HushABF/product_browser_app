1252ef5f263b268b0d4d613a6653c6bd953661a5
HushABF <nkbh12342@gmail.com>
2026-05-02 21:25:36 +0300

    - Added visibilty on/off to password text field

--- lib/features/auth/presentation/widgets/email_and_password_form.dart
+++ lib/features/auth/presentation/widgets/email_and_password_form.dart
@@ -2,35 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:product_browser_app/core/utils/validators.dart';
 import 'package:product_browser_app/core/widgets/app_text_field.dart';
 
-class EmailAndPasswordForm extends StatelessWidget {
+class EmailAndPasswordForm extends StatefulWidget {
   const EmailAndPasswordForm({
     super.key,
     required GlobalKey<FormState> formKey,
     required TextEditingController emailController,
     required TextEditingController passwordController,
-  }) : _formKey = formKey, _emailController = emailController, _passwordController = passwordController;
+  }) : _formKey = formKey,
+       _emailController = emailController,
+       _passwordController = passwordController;
 
   final GlobalKey<FormState> _formKey;
   final TextEditingController _emailController;
   final TextEditingController _passwordController;
 
   @override
+  State<EmailAndPasswordForm> createState() => _EmailAndPasswordFormState();
+}
+
+class _EmailAndPasswordFormState extends State<EmailAndPasswordForm> {
+  bool isObscureText = true;
+  @override
   Widget build(BuildContext context) {
     return Form(
-      key: _formKey,
-    
+      key: widget._formKey,
+
       child: Column(
         children: [
           AppTextFormField(
-            controller: _emailController,
+            controller: widget._emailController,
             hintText: 'Email',
             validator: Validators.validateEmail,
           ),
           SizedBox(height: 32),
           AppTextFormField(
-            controller: _passwordController,
+            controller: widget._passwordController,
             hintText: 'Password',
-            isObscureText: true,
+            isObscureText: isObscureText,
+            suffixIcon: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isObscureText = !isObscureText;
+                });
+              },
+              child: Icon(
+                isObscureText ? Icons.visibility_off : Icons.visibility,
+              ),
+            ),
             validator: Validators.validatePassword,
           ),
         ],
